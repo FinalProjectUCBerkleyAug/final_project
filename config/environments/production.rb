5be613c9ca497ed022ca3ca7299aad0b3e134a4a
MiaaaaaaZ <mirongzou@gmail.com>
2019-12-05 23:38:33 +0000

    devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.action_mailer.default_url_options = {
-    host: 'XXX'
+    host: 'https://ohpet.herokuapp.com/'
   }  
 
   # Code is not reloaded between requests.
