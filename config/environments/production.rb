375caca1c37121088d616398cb99194c7e67e4b3
MiaaaaaaZ <mirongzou@gmail.com>
2019-12-05 23:06:47 +0000

    devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = {
+    host: 'XXX'
+  }  
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
