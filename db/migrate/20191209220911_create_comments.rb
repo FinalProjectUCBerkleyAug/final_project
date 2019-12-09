304b7cad984d3475a5066ab090293ee88ad71c2c
MiaaaaaaZ <mirongzou@gmail.com>
2019-12-09 23:14:43 +0000

    comments db

---
+++ db/migrate/20191209220911_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.text :message
+      t.integer :user_id
+      t.integer :post_id
+      t.timestamps
+    end
+
+    add_index :comments, [:user_id, :post_id]
+    add_index :comments, :post_id
+  end
+end
