749c93a8c6d4cfacfa5408d48dc02dfd573deea9
MiaaaaaaZ <mirongzou@gmail.com>
2019-12-30 18:30:38 +0000

    connected user and pet

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :pets
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
   resources :posts do
