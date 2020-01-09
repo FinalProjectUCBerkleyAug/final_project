d2b1522e1774ffc85639785e0c593a94ffcbd023
MiaaaaaaZ <mirongzou@gmail.com>
2020-01-09 23:42:06 +0000

    quotation error/pushing heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
   config.fog_public = false
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AKIA2RZL2DUHYE2JXUUV"],        # required
-    aws_secret_access_key: ENV["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"],        # required
+    aws_access_key_id:     ENV['AKIA2RZL2DUHYE2JXUUV'],        # required
+    aws_secret_access_key: ENV['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'],        # required
   }
   config.fog_directory  = ENV["pet-social-media"]              # required
 end             
