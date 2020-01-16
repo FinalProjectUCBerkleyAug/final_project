4875e13f1f6902410046a4b7d836ce4c8626180a
Melanie Locke <melanie.a.locke@gmail.com>
2020-01-16 02:03:02 +0000

    Add link to username on pets to follow page, add favors counter to profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
 
   def show
     @user = User.find(params[:id])
     @pets = Pet.includes(:posts).where(user_id: current_user)
+    @favors = @user.favors.count
   end
 
 end
